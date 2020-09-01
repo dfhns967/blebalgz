67e9e81fd264d862a015b1f5d2b53996b667948a
dfhns967 <65913927+dfhns967@users.noreply.github.com>
2020-09-02 02:32:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4294,10 +4294,15 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content.startWith == "$kiss") {
+    if (message.content.startsWith == "$kiss") {
 	    let loved = message.mentions.users.first();
 	    let lover = message.author;
-	    message.channel.send("'${loved} you got a kiss from ${lover}'");
+	    let embed = new Discord.RichEmbed()
+	    .setColor('RANDOM')
+	    .setTitle('`${loved} you got a kiss from ${lover}`')
+	    .set_image(url='https://tenor.com/view/happy-sugar-life-kiss-forehead-cute-anime-sleeping-gif-16465169embed')
+	    
+	    message.channel.send(embed);
     }
 });
 
